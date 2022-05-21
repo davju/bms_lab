3027e5cb9bc3da3b8be63a4fd718079b157f7e81
David Jung <davidjung@Air-von-David.fritz.box>
2022-05-21 09:07:10 +0200

    added millis test for method timing

--- BMS.ino
+++ BMS.ino
@@ -41,8 +41,6 @@ class Dataclass{
       for(int i = 0; i<4;i++){
        cellVoltages[i] = getCellVoltage(i+1);
       }
-
-      Serial.println("Cell voltages updated");
     }
 
     void updateCellTemp(){
@@ -86,7 +84,6 @@ class Safety:protected Dataclass{
 
     void checkForOverTemp(){
       updateData();
-      updateData();
       for(int i=0; i++; i<4){
         if(cellTemp[i] > 50 || cellTemp[i] < 15){
           setWarningOvertemp(true);
@@ -99,17 +96,59 @@ class Safety:protected Dataclass{
 
 
 class Balancing: protected Dataclass{
-  
+  private:
+    double sortedArray[4];
+    double cellDrift;
+    unsigned long triggerTime;
+
+    void calculateCellDrift(){
+        copy();
+        sort();
+        cellDrift = sortedArray[3] - sortedArray[0];
+    }
+
+    void copy(){
+      for(int i=0; i<4; i++){
+        sortedArray[i]=cellVoltages[i];
+      }
+    }
+
+    void sort(){
+      double temp;
+      int j, i;
+      
+      for(i = 0; i<10; i++) {
+        for(j = i+1; j<10; j++){
+          if(sortedArray[j] < sortedArray[i]){
+            temp = sortedArray[i];
+            sortedArray[i] = sortedArray[j];
+            sortedArray[j] = temp;
+          }
+        }
+      }
+    } 
+
+    public:
+      void checkForBalancing(){
+        calculateCellDrift();
+
+        while(cellDrift > 0.2){
+          calculateCellDrift();
+        }
+      }
 };
+
 
 Safety safetyController;
 
+Balancing balancer;
 
 void setup() {
   setupBSW();
 }
 
 void loop() { 
+  static uint32_t oldtime = millis();
   setBDU_Activation(true);   // schaltet BDU ein
   setDriveMode(1);           // 1-Cycle Test 2-Slow Driver 3-Fast Driver 4-Power Mode
   receiveAndParseCommands();   // Empfängt Befehle über den Serial Monitor und führt diese aus
@@ -119,9 +158,17 @@ void loop() {
   //showOCVcurve();
 
   safetyController.checkForVoltageOutOfRange();
-  //setWarningUndervoltage(true);
+  safetyController.checkForOverTemp();
 
-  delay(500);
+  if((millis()-oldtime)>10){
+      oldtime = millis();
+      Serial.println("Sucess"); 
+  }
+
+
+  //setWarningUndervoltage(true);
+  
+  delay(1);
 
            // Stellt OCV Kurve der Li-Ionen Zellen dar
 }
